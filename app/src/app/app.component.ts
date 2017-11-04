67817aca928591819d4b109191162332dd9c42f5
Cleybson Cardoso <cleybsonc@gmail.com>
2017-11-04 00:47:47 -0200

    pegando

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
+import { HistoricoPage } from '../pages/historico/historico';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+  rootPage: any = HistoricoPage;
 
   constructor(
     platform: Platform, statusBar: StatusBar,
